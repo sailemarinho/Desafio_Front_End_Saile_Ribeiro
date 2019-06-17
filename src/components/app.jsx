7001d3113ecf000795c1f10e0c3cd777140f74fc
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-06-16 23:57:07 -0300

    Fetch API name/images

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,12 +5,42 @@ import Header from './header.jsx';
 import Hero from './hero.jsx';
 
 class App extends Component {
+  // state = {
+  //   courses: [],
+  // }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+      imageURL: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://5b7570f8deca780014ec9f86.mockapi.io/v1/cursos`)
+      .then(res => {
+        const courses = res.data;
+        this.setState({ courses, imageURL: courses.imagem });
+      })
+  }
+
   render() {
+    const { imageURL } = this.state;
     return (
       <div>
         <Header />
         <Hero />
-        <div className="container"></div>
+        <div className="container">
+          <ul>
+          { this.state.courses.map(course =>
+            <li key={course.id}>
+              {course.nome}
+              <img src={ course.imagem } />
+            </li>
+            )};
+          </ul>
+        </div>
       </div>
 
     );
